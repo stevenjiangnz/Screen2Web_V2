fbe29eb5dfe6ebab94a856543c5eae8c92283147
Steven Jiang <stevenjiangnz@gmail.com>
2017-06-18 15:41:55 +1000

    add top menu and placeholders

---
+++ src/app/stock/stock.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { StockListComponent } from './stock-list.component';
+
+export const stockRoutes: Routes = [
+    {path: 'stock', component: StockListComponent}
+];
