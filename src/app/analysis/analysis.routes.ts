15d0028ef10ad6fc9fc0c6c50591d93227838bbd
Steven Jiang <stevenjiangnz@gmail.com>
2017-10-07 12:29:07 +1000

    about to add datatable

--- src/app/analysis/analysis.routes.ts
+++ src/app/analysis/analysis.routes.ts
@@ -6,7 +6,7 @@ import { RuleComponent } from './rule/rule.component';
 export const analysisRoutes: Routes = [
     {path: 'analysis', component: AnalysisComponent,
         children: [
-            {path: '', redirectTo: 'zone', pathMatch: 'full'},
+            {path: '', redirectTo: 'rule', pathMatch: 'full'},
             {path: 'zone', component: ZoneComponent},
             {path: 'rule', component: RuleComponent}
         ]},
