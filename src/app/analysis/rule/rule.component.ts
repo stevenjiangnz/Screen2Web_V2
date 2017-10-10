dff3eecd305e9e554363fa33594d7771ed1276a1
Steven Jiang <stevenjiangnz@gmail.com>
2017-10-10 22:17:37 +1000

    add datatable works

--- src/app/analysis/rule/rule.component.ts
+++ src/app/analysis/rule/rule.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-rule',
   templateUrl: './rule.component.html',
   styleUrls: ['./rule.component.scss']
 })
-export class RuleComponent implements OnInit {
+export class RuleComponent implements OnInit, AfterViewInit {
 
 
   constructor() { }
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const exampleId: any = $('#example');
+
+    // exampleId.hide();
+    console.log(exampleId);
+    exampleId.DataTable();
   }
 }
