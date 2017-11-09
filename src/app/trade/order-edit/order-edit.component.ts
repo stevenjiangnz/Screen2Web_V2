6b27e2d6ab50fbb6ef95dbd1f29173a7929a7b1c
Steven Jiang <stevenjiangnz@gmail.com>
2017-11-09 20:35:41 +1000

    add node_modules

--- src/app/trade/order-edit/order-edit.component.ts
+++ src/app/trade/order-edit/order-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { INgxMyDpOptions, IMyDateModel } from 'ngx-mydatepicker';
+import { ObjHelper } from '../../utils/obj-helper';
+import { ToasterService } from 'angular2-toaster';
+import { TradeService } from '../../services/trade.service';
 
 @Component({
   selector: 'app-order-edit',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-edit.component.scss']
 })
 export class OrderEditComponent implements OnInit {
+  orderForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _toasterService: ToasterService, private _tradeService: TradeService) { 
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.orderForm = this.fb.group({
+      name: ['', Validators.required],
+    });
   }
 
 }
